369523509c06486a8f953d7c99bb6e745733c2f8
elisad5791 <elisad5791@mail.ru>
2022-02-26 15:29:41 +0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readAndParse from './parsers.js';
 
 const getDiff = (obj1, obj2) => {
   const iter = (tree1, tree2, path) => {
-    const tree = Object.assign({}, tree1, tree2);
+    const tree = { ...tree1, ...tree2 };
     const keys = _.sortBy(Object.keys(tree));
     return keys.reduce((acc, key) => {
       const newPath = path.concat(key);
@@ -11,13 +11,13 @@ const getDiff = (obj1, obj2) => {
       const value2 = _.get(obj2, newPath);
       const keyDel = `${key}_del`;
       const keyAdd = `${key}_add`;
-      if(!_.isPlainObject(value1) || !_.isPlainObject(value2)) {
-        if (!_.has(obj1, newPath)) return Object.assign({}, acc, { [keyAdd]: value2 });
-        if (!_.has(obj2, newPath)) return Object.assign({}, acc, { [keyDel]: value1 });
-        if (value1 === value2) return Object.assign({}, acc, { [key]: value1 });
-        return Object.assign({}, acc, { [keyDel]: value1 }, { [keyAdd]: value2 });
+      if (!_.isPlainObject(value1) || !_.isPlainObject(value2)) {
+        if (!_.has(obj1, newPath)) return { ...acc, [keyAdd]: value2 };
+        if (!_.has(obj2, newPath)) return { ...acc, [keyDel]: value1 };
+        if (value1 === value2) return { ...acc, [key]: value1 };
+        return { ...acc, [keyDel]: value1, [keyAdd]: value2 };
       }
-      return Object.assign({}, acc, { [key]: iter(value1, value2, newPath) });
+      return { ...acc, [key]: iter(value1, value2, newPath) };
     }, {});
   };
 
@@ -31,11 +31,10 @@ const pad = (txt, count) => txt.split('\n')
 const stylish = (obj, start = false) => {
   const entries = Object.entries(obj);
   const body = entries.map(([key, value]) => {
-    const newKey = key.endsWith('_add') ? `+ ${key.replace('_add', '')}` :
-                   key.endsWith('_del') ? `- ${key.replace('_del', '')}` :
-                   `  ${key}`;
     const newValue = _.isPlainObject(value) ? stylish(value) : value;
-    return `${newKey}: ${newValue}`;
+    if (key.endsWith('_add')) return `+ ${key.replace('_add', '')}: ${newValue}`;
+    if (key.endsWith('_del')) return `- ${key.replace('_del', '')}: ${newValue}`;
+    return `  ${key}: ${newValue}`;
   }).join('\n');
   const count = start ? 2 : 4;
   const end = start ? '' : '  ';
