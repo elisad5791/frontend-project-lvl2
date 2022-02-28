cfd217e5e422d309459c6ebc803534a090a3877d
elisad5791 <elisad5791@mail.ru>
2022-02-28 20:14:47 +0500

    Fix errors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import _ from 'lodash';
 import readAndParse from './parsers.js';
 import getFormatter from './formatters/index.js';
 
+const getStatus = (inFirst, inSecond, value1, value2, isNotChangable) => {
+  if (isNotChangable) return 'saved';
+  if (!inFirst) return 'added';
+  if (!inSecond) return 'removed';
+  if (value1 === value2) return 'saved';
+  return 'updated';
+};
+
 const getDiff = (tree1, tree2) => {
   const iter = (obj1, obj2, path, isNotChangable) => {
     const newObj1 = _.isPlainObject(obj1) ? _.cloneDeep(obj1) : {};
@@ -19,22 +27,12 @@ const getDiff = (tree1, tree2) => {
       const value1 = isObj1 ? '[complex value]' : val1;
       const value2 = isObj2 ? '[complex value]' : val2;
 
-      const record = [];
-      record[0] = newPath;
-      record[1] = value1;
-      record[2] = value2;
-
-      if (isNotChangable) {
-        record[3] = 'saved';
-      } else if (!_.has(tree1, newPath)) {
-        record[3] = 'added';
-      } else if (!_.has(tree2, newPath)) {
-        record[3] = 'removed';
-      } else if (value1 === value2) {
-        record[3] = 'saved';
-      } else {
-        record[3] = 'updated';
-      }
+      const inFirst = _.has(tree1, newPath);
+      const inSecond = _.has(tree2, newPath);
+      const record = [newPath, 
+                      value1,
+                      value2,
+                      getStatus(inFirst, inSecond, value1, value2, isNotChangable)];
 
       if (value1 !== '[complex value]' && value2 !== '[complex value]') {
         return [record];
