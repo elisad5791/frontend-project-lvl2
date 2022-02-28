626117c40209bdb7578a3693232a66f6cc7d8364
elisad5791 <elisad5791@mail.ru>
2022-02-28 19:29:13 +0500

    Fix errors

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,22 +1,21 @@
 const formatPlain = (diff) => {
-  const output = diff.reduce((acc, item) => {
-    const status = item[3];
-    if (status === 'saved') {
-      return acc;
-    }
+  const changes = diff.filter((item) => item[3] !== 'saved')
+    .map((item) => {
+      const status = item[3];
 
-    const path = item[0].join('.');
-    const firstVal = (typeof item[1] === 'string' && item[1] !== '[complex value]') ? `'${item[1]}'` : item[1];
-    const secondVal = (typeof item[2] === 'string' && item[2] !== '[complex value]') ? `'${item[2]}'` : item[2];
+      const path = item[0].join('.');
+      const firstVal = (typeof item[1] === 'string' && item[1] !== '[complex value]') ? `'${item[1]}'` : item[1];
+      const secondVal = (typeof item[2] === 'string' && item[2] !== '[complex value]') ? `'${item[2]}'` : item[2];
 
-    const commonPart = `${acc}Property '${path}' was ${status}`;
-    const addedPart = ` with value: ${secondVal}`;
-    const updatedPart = `. From ${firstVal} to ${secondVal}`;
+      const commonPart = `Property '${path}' was ${status}`;
+      const addedPart = ` with value: ${secondVal}`;
+      const updatedPart = `. From ${firstVal} to ${secondVal}`;
 
-    if (status === 'added') return `${commonPart}${addedPart}\n`;
-    if (status === 'updated') return `${commonPart}${updatedPart}\n`;
-    return `${commonPart}\n`;
-  }, '');
+      if (status === 'added') return `${commonPart}${addedPart}`;
+      if (status === 'updated') return `${commonPart}${updatedPart}`;
+      return `${commonPart}`;
+    }, '');
+  const output = changes.join('\n');
   return output;
 };
 
