7186b83755241b5e1162aafe5a030128461230c1
elisad5791 <elisad5791@mail.ru>
2022-03-16 12:09:01 +0500

    Fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,21 +10,19 @@ const getFixturesPath = (filename) => {
   return path.join(__dirname, '..', '__fixtures__', filename);
 };
 
-let resultStylish;
-let resultPlain;
-let resultJson;
+const result = {};
 
 beforeAll(() => {
-  resultStylish = readFileSync(getFixturesPath('result_stylish.txt'), 'utf8');
-  resultPlain = readFileSync(getFixturesPath('result_plain.txt'), 'utf8');
-  resultJson = readFileSync(getFixturesPath('result_json.txt'), 'utf8');
+  result.stylish = readFileSync(getFixturesPath('result_stylish.txt'), 'utf8');
+  result.plain = readFileSync(getFixturesPath('result_plain.txt'), 'utf8');
+  result.json = readFileSync(getFixturesPath('result_json.txt'), 'utf8');
 });
 
 test.each(['json', 'yml'])('gendiff', (ext) => {
   const filename1 = getFixturesPath(`file1.${ext}`);
   const filename2 = getFixturesPath(`file2.${ext}`);
 
-  expect(gendiff(filename1, filename2, 'stylish')).toEqual(resultStylish);
-  expect(gendiff(filename1, filename2, 'plain')).toEqual(resultPlain);
-  expect(gendiff(filename1, filename2, 'json')).toEqual(resultJson);
+  expect(gendiff(filename1, filename2, 'stylish')).toEqual(result.stylish);
+  expect(gendiff(filename1, filename2, 'plain')).toEqual(result.plain);
+  expect(gendiff(filename1, filename2, 'json')).toEqual(result.json);
 });
